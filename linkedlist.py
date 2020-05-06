0513740d00f6e9af21e497372fadbd8180dce2f0
Brandon Forster <me@brandonforster.com>
2020-05-06 15:09:44 -0500

    Cheated and used a reverse algo from the internet.
    Signed-off-by: Brandon Forster <me@brandonforster.com>

--- linkedlist.py
+++ linkedlist.py
@@ -7,8 +7,10 @@ class LinkedList:
 
     def tail(self):
         pointer = self.head
+
         while pointer.next is not None:
             pointer = pointer.next
+
         return pointer
 
     def add(self, new: node):
@@ -18,9 +20,28 @@ class LinkedList:
 
     def delete(self, data):
         pointer = self.head
+
         while pointer.next.data is not data:
             pointer = pointer.next
+
         pointer.next = pointer.next.next
 
     def reverse(self):
-        return None
+        # no need to reverse if head is null
+        # or there is only 1 node.
+        if self.head is None or self.head.next is None:
+            return self.head
+
+        list_to_do = self.head.next
+
+        reversed_list = self.head
+        reversed_list.next = None
+
+        while list_to_do is not None:
+            temp = list_to_do
+            list_to_do = list_to_do.next
+
+            temp.next = reversed_list
+            reversed_list = temp
+
+        return reversed_list
