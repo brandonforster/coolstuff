fca5494b10a39e64234e98d51c3c3acca17b0421
Brandon Forster <me@brandonforster.com>
2020-06-10 18:04:11 -0500

    This doesn't work but it's a nice jump off.
    Signed-off-by: Brandon Forster <me@brandonforster.com>

--- gridops.py
+++ gridops.py
@@ -2,4 +2,13 @@ from typing import List
 
 
 def rotate_image(img: List[List[int]]) -> List[List[int]]:
-    return img
+    width = len(img[0])
+    height = len(img)
+
+    rotated = [[0 for x in range(width)] for y in range(height)]
+
+    for x in range(0, width):
+        for y in range(0, height):
+            rotated[x][y] = img[width-x][height-y]
+
+    return rotated
